7b66218f0ccede46e05bc87e7c3be0d32c8856e8
Mista Log <oloyedeoibrahimsmile@gmail.com>
2025-02-19 21:27:06 +0100

    Configured models.py each

--- alerts/models.py
+++ alerts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class AlertRule(models.Model):
+    pass
+
+
+
+
+class NotificationChannel(models.Model):
+    pass
--- security/models.py
+++ security/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class SecurityEvent(models.Model):
+    pass
+
+
+class VulnerabilityScan(models.Model):
+    pass
