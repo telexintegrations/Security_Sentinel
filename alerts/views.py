f125dce53bd08fa5ef4b4a43184607481c851231
Mista Log <oloyedeoibrahimsmile@gmail.com>
2025-02-23 10:43:00 +0100

    Created alert app

---
+++ alerts/views.py
@@ -0,0 +1,7 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from django.http import JsonResponse
+from django.views import View
+
