8d209ec8ebbb6f99f241be3d6cd077da14964210
Mista Log <oloyedeoibrahimsmile@gmail.com>
2025-02-23 10:04:33 +0100

    Started core app

--- Security_sentinel/settings.py
+++ Security_sentinel/settings.py
@@ -42,8 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-
-
+    'rest_framework',
+    'core',
 ]
 
 REST_FRAMEWORK = {
---
+++ core/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import CoreIntegration
+from .models import SecurityIntegration
+from .models import VulnerabilityScan
+from .models import SecurityEvent
+
+
+
+
+# Register your models here.
